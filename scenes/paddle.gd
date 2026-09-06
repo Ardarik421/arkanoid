796addbb98c59f6cfa59acc4e306387dee70f28a
Artem Troshkin <90347521+Ardarik421@users.noreply.github.com>
2026-09-06 17:24:20 +0300

    Добавил управление мышью

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -6,11 +6,13 @@ extends CharacterBody2D
 
 @export var left_limit: float = 20.0
 @export var right_limit: float = 940.0
+@export var mouse_speed: float = 1800.0
 
+var can_move: bool = true
+var use_mouse_control: bool = false
 
 func _ready():
 	queue_redraw()
-
 
 func _draw():
 	var rect = Rect2(
@@ -20,24 +22,42 @@ func _draw():
 
 	draw_rect(rect, Color.WHITE)
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		use_mouse_control = true
 
 func _physics_process(delta):
 	if not can_move:
 		velocity = Vector2.ZERO
 		return
-		
-	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
-	velocity.y = 0
 
-	move_and_slide()
+	var direction = Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
+		use_mouse_control = false
+		velocity.x = direction * speed
+		velocity.y = 0
+		move_and_slide()
+
+	elif use_mouse_control:
+		var target_x = get_global_mouse_position().x
+		var distance = target_x - global_position.x
+
+		velocity.x = clamp(
+			distance / delta,
+			-mouse_speed,
+			mouse_speed
+		)
+
+		velocity.y = 0
+		move_and_slide()
+
+	else:
+		velocity = Vector2.ZERO
 
 	var half_width = width / 2.0
-
 	global_position.x = clamp(
 		global_position.x,
 		left_limit + half_width,
 		right_limit - half_width
 	)
-
-var can_move: bool = true
