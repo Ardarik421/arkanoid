ffaebe0291c6e3424ddabbd38cc6f5e7cb6787f2
Artem Troshkin <90347521+Ardarik421@users.noreply.github.com>
2026-09-08 22:23:17 +0300

    Подключить выбор уровня к главному меню

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,9 +1,11 @@
 extends Control
 
 const GAME_SCENE: String = "res://scenes/main.tscn"
+const LEVEL_SELECT_SCENE: String = "res://scenes/level_select.tscn"
 
 func _ready():
 	$Menu/ContinueButton.disabled = SaveManager.highest_unlocked_level <= 1
+	$Menu/LevelSelectButton.disabled = SaveManager.highest_unlocked_level < 5
 	$Menu/NewGameButton.grab_focus()
 
 func _on_new_game_pressed():
@@ -14,6 +16,9 @@ func _on_new_game_pressed():
 func _on_continue_pressed():
 	SaveManager.selected_level = SaveManager.highest_unlocked_level
 	get_tree().change_scene_to_file(GAME_SCENE)
+
+func _on_level_select_pressed():
+	get_tree().change_scene_to_file(LEVEL_SELECT_SCENE)
 
 func _on_exit_pressed():
 	get_tree().quit()
